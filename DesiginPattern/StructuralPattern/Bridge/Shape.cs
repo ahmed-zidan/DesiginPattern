df82d3a64373a4c6308f1fd04e1a8daa2411c4a1
ahmed zidan <zidanzezo17@gmail.com>
2024-04-04 17:25:51 +0200

    Bridge

---
+++ DesiginPattern/StructuralPattern/Bridge/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesiginPattern.StructuralPattern.Bridge
+{
+  public abstract class Shape
+    {
+       protected IColor color;
+        protected Shape(IColor color)
+        {
+            this.color = color;
+        }
+    }
+}
---
+++ DesiginPattern/StructuralPattern/Bridge/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesiginPattern.StructuralPattern.Bridge
+{
+    class Square :Shape
+    {
+       
+        public Square(int x,IColor color):base(color)
+        {
+            
+        }
+
+    }
+}
